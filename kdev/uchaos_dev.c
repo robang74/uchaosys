40ef65e314a327afa52bb24e97e7cefce5064c02
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-03-20 16:12:00 +0100

    kdev/uchaos_dev.h,uckaos.c: bringing .ko's algo in userland
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- kdev/uchaos_dev.c
+++ kdev/uchaos_dev.c
@@ -103,8 +103,6 @@ static DEFINE_MUTEX(uchaos_lock);
 
 // --- Fuctional Definitions ---
 
-#define abs_t(t, x) ({ t __x = (x); (__x < 0) ? -__x : __x; })
-
 #define MAX_INPUT_SIZE (1024 << 3)
 
 #define AB  (6)
@@ -115,18 +113,16 @@ static DEFINE_MUTEX(uchaos_lock);
 #define ABy ((ABN>>2)-1)  //  7 or 15
 #define ABz ((ABN>>3)-1)  //  3 or  7
 
-#define murmul1 0xff51afd7ed558ccdULL
-#define murmul2 0xc4ceb9fe1a85ec53ULL
-#define rot1    47
-#define rot2    17
-#define rot3    13
-
-#define getprmx16(w) (5 + (((w) & ABy) << 1))
-#define getprmx(val) (primes64[getprmx16((val))])  // because %10 is slower
 #define HASH_SEED 14695981039346656037ULL
-#define MULTIPLIER 0xff51afd7ed558ccdULL
 #define HASHSIZE (ABN >> 3)
+
+#if 0
+static const u8 primes64[20] = {  3, 61,  5, 59, 11, 53, 17, 47, 23, 41,
+                                 19, 45, 29, 35, 31, 33, 13, 51,  7, 57 };
+#define getprmx(val) (primes64[getprmx16((val))])  // because %10 is slower
+#define MULTIPLIER 0xff51afd7ed558ccdULL
 #define LSHIFT (ABx+2)
+#endif
 
 // --- Fuctional Declarations ---
 
@@ -134,206 +130,10 @@ typedef u64 __attribute__((aligned(HASHSIZE))) archul_t;
 
 static archul_t *kbuf = NULL; // Stack allocation, one char device only
 
-static const u8 primes64[20] = {  3, 61,  5, 59, 11, 53, 17, 47, 23, 41,
-                                 19, 45, 29, 35, 31, 33, 13, 51,  7, 57 };
-
-static inline archul_t rotlbit(archul_t n, u8 c) {
-    c &= ABX; return (n << c) | (n >> ((-c) & ABX));
-}
-
-static inline archul_t knuthmx(archul_t iw) {
-    register archul_t w = iw;
-    w  = rotlbit(w, getprmx16(w));
-    w *= (w & 1) ? 0x9E3779B9 : 0x045d9f3b;
-    w ^= rotlbit(w, (w & 2) ? rot1 : rot2);
-    return w;
-}
-
-static inline archul_t murmux3(archul_t ks, archul_t p)
-{
-    register archul_t z = ks;
-    z =  p ^ ((z >> (ABx-2)) * murmul1);
-    z = (z ^ ( z <<  ABx  )) * murmul2;
-    z =  z ^ ( z >> (ABx+2));
-    return z;
-}
-
-#define dtskew(x) (!x || (x)>>28)    // 2^29 is the biggest 2^n before 1E9
-
-#define ONESEC msecs_to_jiffies(1<<10)
-
 #define prtkinfo(x...) { if(verbosity) printk(KERN_INFO MODULE_NAME ": " x); }
 
-/*
- * ATOMICITY ON A 1CPU vs SMP SYSTEM: the 'loop_failure' flag is read in many
- * places, but wrote in one only: 'volatile' was fine, 'atomic_t' is the way.
- * However also failure_jiff requires multi-thread protection, by 'uchaos_lock'.
- * Because 'uchaos_lock' protects writes, reading a 'volatile bool' is safe.
- * The 'volatile' isn't a SMP memory barrier as we expect but each CPU core
- * cache therefore for the most general implementation 'atomic_t' is the way.
- */
-static atomic_t loop_failure = ATOMIC_INIT(0);
-
-static inline archul_t djb2tum(archul_t seed, size_t num)
-{
-    static unsigned long failure_jiff = 0;
-    static archul_t dmx = 0, dmn = -1, mavg = 0, ohs = HASH_SEED;
-
-#ifdef _PROVIDE_STATS
-    static u64 nexp = 0, evnt = 0, ncl = 0, tcyl = 0, nhsh = 0;
-    static archul_t avg = 0, jmn = -1, jmx = 0;
-#endif
-    volatile int i, j = 0; // volatile as current CPU memory barrier in the loop
-    register archul_t ent = 0, hsh = ohs; // these two in particular need accel.
-    archul_t tns, dlt, dff, ons = 0;
-    u8 b0, b1, excp = 0;
-
-    /*
-     * RATIONALE: also flooding the system of printks isn't a good idea, after all.
-     * There is not an easy way to fall in this "SYSBUG" but also not an easy way to
-     * deal with it because it is not within the coding/logic of this driver's scope.
-     */
-    if( atomic_read( &loop_failure ) ) {
-        if ( time_after(jiffies, failure_jiff + ONESEC) ) {
-            failure_jiff = 0;
-            atomic_set(&loop_failure, 0);
-        } else goto enforcedquit;
-    }
-
-    if( seed ) hsh ^= seed;
-    else { ons = ent = 0; }
-
-    if( !ons ) {
-        ons = ktime_get_ns();
-        hsh = knuthmx(hsh ^ ons);
-    }
-
-    for (i = 0; i < num; i++) {
-        if( ent && hsh ) ent ^= rotlbit(ent, getprmx16(hsh));
-/* -----------------------------------------------------------------------------
- * WARNING:
- * it might loop forever, because of a BUG rather than falling in a corner case
- * -------------------------------------------------------------------------- */
-reschedule:
-        /*
-         * RATIONALE: we cannot ignore that in some extreme conditions this code can
-         * create a livelock rescheduling for an unlimited number of times. Something
-         * exotic like ktime_get_ns() function pointer was corrupted in a way that it
-         * returns always the same value. The expectation is 3-12 range of reschedules
-         * for each function cold-call. When 1% might require 100x more, performance
-         * is halved and it is a degradation of the service but never a lock. Hopefully,
-         * we never see this kind of failure in a production system. In critical systems
-         * a lock/hack by ktime_get_ns() can cost a disaster, not just low-quality entropy
-         * or scarcity. Anyway, when ktime_get_ns() systematically fails much probably
-         * other parts of the kernel would create DoS or SysFail in such a way that
-         * uChaos will be the least of the issues. Not being a critical one, is enough.
-         */
-        // 2^10 is a large arbitrary value, don't overlook 'arbitrary' when coding
-        if( (++j) >> 10 ) {
-            failure_jiff = jiffies;
-            #define UCWRN MODULE_NAME ": EMERGENCY ABORT - "
-            printk(KERN_ALERT UCWRN "potential infinite reschedule loop!\n");
-            if(verbosity)
-            prtkinfo("Abort w/ loops=%d,%d kbuf_offset=0x%02lx, jiffies=%lu\n",
-                i, j, (unsigned long)kbuf & 255, jiffies);
-            atomic_set(&loop_failure, 1);
-            // A more drastic way would be unregistering the char device
-            goto enforcedquit;
-        }
-        /*
-         * Immediately after the infinite loop check,
-         * hashing stuff is starting with a CPU nap!
-         */
-        cpu_relax();
-#ifdef _PROVIDE_STATS
-        nexp++;
-#endif
-        do {
-            tns = ktime_get_ns();
-            dlt = tns - ons;
-            if( !dlt ) {                                      goto reschedule; }
-            else ons = tns;
-        } while( dtskew(dlt) );
-
-        if(dmn == -1) { dmn = dlt;                            goto reschedule; }
-
-        if( dlt < dmn ) {
-            dff = dmn - dlt; dmn = dlt;
-            ent ^= -dff ^ dmn;
-#ifdef _PROVIDE_STATS
-            evnt++;
-#endif
-        } else
-        if( dlt > dmx ) {
-            dff = dlt - dmn; dmx = dlt;
-            ent ^= dff ^ -dmx;
-#ifdef _PROVIDE_STATS
-            evnt++;
-#endif
-        } else {
-            dff = dlt - dmn;
-            ent ^= ~dff ^ dmx;
-        }
-        if( !dff ) { hsh = knuthmx(hsh);                      goto reschedule; }
-
-        // dff is jittering for the exeption manager activation
-        if( dff < min_delta + excp ) {
-            excp += 4;                     // excp is accounting vs dff
-#ifdef _PROVIDE_STATS
-            nexp++;
-#endif
-        } else {
-            // Knuth, based on gold section seeded by 1E-3 ~ 1E-4 event idx
-            if( excp ) { hsh = murmux3(hsh, ons); }
-            excp = 0;
-#ifdef _PROVIDE_STATS
-            if( jmn == -1 ) jmn = dff;
-            else
-            if( dff < jmn ) jmn = dff;
-            if( dff > jmx ) jmx = dff;
-            avg += dlt; javg += dff;
-            ncl++;
-#endif
-        }
-
-        // Half of the values above and below expected but few around creates
-        // uncertanty which affects how ent is calculated but not on average
-        // Instead, a trigger by "uncommon" events spawn a ca.10-12bit branch
-        // and this detour apports unpredicatbility not just flat-white noise.
-        #define MAVG(x) (( tns > min_delta ) ? (x) : ~(x))
-
-        tns  = abs_t(u32, dlt - mavg);
-        ent ^= MAVG(dlt)  <<      ABz;       // 1st derivative of time
-        ent ^= MAVG(ons)  <<     rot3;      // current monotonic time
-        ent  = knuthmx(ent ^ MAVG(dff));   // 2nd derivative of time
-
-        b0 = ent & 0x01, b1 = ent & 0x02;
-        hsh = ( hsh << (4 + (b0 ? b1 : 1)) ) + (b1 ? -hsh : hsh);
-        hsh ^= rotlbit( hsh ^ MAVG(tns), getprmx16(ent >> 2) );
-
-        // Moving average, where (mavg * 255) = (mavg + 256 - mavg) but faster
-        mavg = ((mavg << 8) - mavg + dlt) >> 8;
-    }
-#ifdef _PROVIDE_STATS
-    nexp -= num;
-    tcyl += num;
-#endif
-
-    /*
-     * RATIONALE: if 'goto enforcedquit' is enforced, the system is probably done
-     * and near an imminent collapse but this wouldn't allow to creates a DoS here
-     * rather than a soft-degradation of the service quality like doing LCG as RNG.
-     */
-enforcedquit:
-    ent = hsh;                             // forget the entropy mixed in hash
-    hsh = murmux3(hsh, ohs);               // whitening the hash before deliver
-    ohs = ent;                             // keep the hashing internal state
-
-#ifdef _PROVIDE_STATS
-    nhsh++;
-#endif
-    return hsh;
-}
+#define _CHK_LOOP_FAIL
+#include "uchaos_dev.h"
 
 // --- File Operations ---
 
@@ -492,14 +292,7 @@ static int __init uchaos_init(void)
 
     prtkinfo("Init (bb:%d) auxiliary entropy source, quality: %d\n",
         badb_init, entr_qlty);
-    ebuf[0] = ktime_get_ns();
-    ebuf[0] = djb2tum(ebuf[0], loop_mult * init_runs);
-    // by default settings, the previous call with init_runs brings in variance
-    ebuf[1] = ktime_get_ns();
-    ebuf[1] = djb2tum(ebuf[1], loop_mult);
-    // by default settings, further calls with loop_mult have a smaller variance
-    ebuf[2] = djb2tum(0,       loop_mult);
-    ebuf[3] = djb2tum(0,       loop_mult);
+    __init4_djb2tum(ebuf);
 
     /* -------------------------------------------------------------------- */ {
     size_t len = sizeof(ebuf);

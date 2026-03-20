f3f0986d171d7750c93624964b0b946342cdf7b9
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-03-20 12:23:06 +0100

    qemu/start.sh: simpler and cope with initramfs updating
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- qemu/start.sh
+++ qemu/start.sh
@@ -1,105 +1,75 @@
 #!/bin/bash
+#
 # (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, MIT license
+#
 
-qemubin="qemu-system-x86_64"
 append_for_kernel_debug="earlyprintk=serial nokaslr -pidfile vm.pid -panic=1"
+
+kimg="bzImage"
+rfsimg="initramfs.cpio"
+qemubin="qemu-system-x86_64"
+
+test -r ${rfsimg}.gz && rfsimg="${rfsimg}.gz"
+rfsdir=$(echo "$rfsimg" | sed 's/\.cpio\.gz//;s/\.cpio//')
+
+chkmd5() { md5sum -c update/$rfsdir.md5 2>/dev/null; }
 
 # Cope with the user's parametric input
 
 test -r bzImage || ln -sf bzImage.orig bzImage
 
-docpio=1
-update=0
-tstimg=0
+# Command line flags management
 
-cmdlnx=
-if [ "x${1:-}" = "x-z" ]; then
-  export QZERO=1 QMSZE=256M UCTEST=${UCTEST:-0}
-  cmdlnx="UCTEST=$UCTEST"
-  shift
-elif [ "x${1:-}" = "x-Z" ]; then
+imgupdte=0
+zerokelv=0
+zerotest=0
+quietrun=0
+
+while [ $# -gt 0 ]; do
+  case "x$1" in
+    "x-u")
+      imgupdte=1
+      shift
+      ;;
+    "x-z")
+      zerokelv=1
+      shift
+      ;;
+    "x-Z")
+      zerotest=1
+      shift
+      ;;
+    "x-q")
+      quietrun=1
+      shift
+      ;;
+    *)
+      echo "Unknown flag, ignored: $1"
+      shift
+      ;;
+  esac
+done
+
+if [ $imgupdte -ne 0 ]; then
+  sh -c "cd ..; ./cpio.sh -c"
+  read -p ">>> Updated, press ENTER to continue " key
+fi
+
+if [ $quietrun -ne 0 ]; then
+  cmdlnx="quiet"
+else
+  cmdlnx=""
+fi
+
+if [ $zerotest -ne 0 ]; then
   export QZERO=1 QMSZE=256M UCTEST=${UCTEST:-1}
   cmdlnx="UCTEST=${UCTEST:-1}"
-  shift
-else
-  test -n "$UCTEST" && cmdlnx="UCTEST=$UCTEST"
+elif [ $zerokelv -ne 0 ]; then
+  export QZERO=1 QMSZE=256M UCTEST=${UCTEST:-0}
+  cmdlnx="UCTEST=${UCTEST:-1}"
+elif [ -n "$UCTEST" ]; then
+  cmdlnx="UCTEST=$UCTEST"
 fi
-
-if [ "x${1:-}" = "x-t" ]; then
-  tstimg=1
-  shift;
-elif [ "x${1:-}" = "x-u" ]; then
-  update=1
-  tstimg=1
-  shift;
-fi
-
-if [ "x${1:-}" = "x-T" ]; then
-  docpio=0
-  shift;
-elif [ "x${1:-}" = "x-U" ]; then
-  {
-    echo | sh $0 -t; echo | sh $0 -t -r
-    echo | sh $0 -u; echo | sh $0 -u -r
-  } >/dev/null
-  echo
-  for f in update/initr*md5; do
-    sed -e "s/.*uchaos.gz.sh$//" -e "s/.*RNG_.*static$//" -i $f
-    echo "$f:"; cat $f | grep .
-    echo
-  done
-  exit
-fi
-
-if [ "x${1:-}" = "x-r" ]; then
-  rfsimg="initrobfs.cpio"
-  shift; set -- "$rfsimg" "$@"
-else
-  rfsimg="${1:-initramfs.cpio}"
-fi
-test -r ${rfsimg}.gz && rfsimg="${rfsimg}.gz"
-
-kimg="${2:-bzImage}"
-
-tmpdir=${3:-}
-test "$tmpdir" = "cpio" && tmpdir=""
-if [ ! -n "$tmpdir" ]; then
-  tmpdir="cpio.tmp/"
-  trap "rm -rf $tmpdir" EXIT INT TERM
-  rm -rf $tmpdir
-fi
-
-# PractRand requires a quite large memory buffer to allocate
-test "$kimg" = "bzImage.515x" && QMSZE=${QMSZE:-512M}
-
-# Updating the image before start it
-
-rfsdir=$(echo "$rfsimg" | sed 's/\.cpio\.gz//;s/\.cpio//')
-chkmd5() { md5sum -c update/$rfsdir.md5 2>/dev/null; }
-
-if [ -d update/$rfsdir/ ]; then
-  printf "Checking is ramfs update "
-  if ! chkmd5 || [ $docpio -ne 0 ]; then
-    sh cpio.sh -e $rfsimg $tmpdir 2>&1 | grep -E "cpio: | blocks"
-    cp -arf -pd update/common/* update/$rfsdir/* $tmpdir/ 2>&1
-    sh cpio.sh -c $rfsimg.new $tmpdir 2>&1
-    rfsimg="$rfsimg.new"
-    if ! chkmd5; then
-      echo "ERROR: ramfs updated doesn't match md5 checksum"
-      echo "       press ENTER to start the QEMU VM anyway."
-      test $update -eq 0 && read x
-    fi
-    rm -rf $tmpdir
-  fi
-fi
-
-test -r $rfsimg.new && rfsimg="$rfsimg.new"
-
-if [ $update -ne 0 ]; then
-  md5sum $(find $rfsimg update/common/ update/$rfsdir/ ! -type d) > update/$rfsdir.md5
-fi
-
-test $tstimg -eq 0 || exit
 
 # Preparing the QEMU virtual machine configuration #############################
 

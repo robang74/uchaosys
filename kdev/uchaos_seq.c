88190fded2952d55b7533ae86a606a76b5e4f15a
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2026-05-09 22:41:57 +0200

    kdev/uchaos_seq.c: v0.0.5
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- kdev/uchaos_seq.c
+++ kdev/uchaos_seq.c
@@ -2,15 +2,18 @@
  * uchaos_seq.c - Character sequencer for uchaos-based jitter hashing
  * (c) 2026, Roberto A. Foglietta <roberto.foglietta@gmail.com>, GPLv2
  */
- #define VERSION "v0.0.4"
+ #define VERSION "v0.0.5"
  /*
  * Compile and run with:
- *     cc -s -g0 -O1 uchaos_seq.c -o ucseq && ./ucseq
+ *   CFLAGS="-s -g0 -O1 -Wno-format-extra-args -I../usrl"
+ *   cc uchaos_seq.c $CFLAGS -o ucseq && ./ucseq
  */
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sched.h>
 
 #include "getnanos.h"
@@ -75,19 +78,39 @@ do {
 
 #endif
 
-int main(void) {
-  int i, n;
-  uint64_t e, t = get_nanos();
+
+static inline uint32_t rotl32(uint32_t x, uint8_t n) {
+  n &= 31; return (x << n) | (x >> (32-n));
+}
+#define rotl5(x) rotl32(x, 5)
+
+static inline uint64_t nanornd(uint64_t e) {
+  uint64_t t;
+  sched_yield();
+  t = get_nanos();
+  return t ^ (e * t);
+}
+
+#define newln1()       if(!argn) { putchar('\n'); }
+#define print1(fmt...) if(!argn) { fprintf(stdout, fmt); }
+#define print2(fmt...) if(!argn) { fprintf(stderr, fmt); }
+#define write4(x)      if( argn) { ssize_t w = write(1, &x, 4); }
+
+int main(int argc, char *argv[]) {
+  uint64_t e = get_nanos();
+  uint32_t i, n, r, argn = 0;
   uint8_t bytes[256], count[256], nbits[256];
   uint8_t goods[128], table[256], nb[4], c;
 
-  fprintf(stderr,
-    "\n//  Executing %s in %s\n",
-    __FILE__, VERSION);
-  putchar('\n');
+  if(argc & 2)
+    argn = atol(argv[1]);
 
-  sched_yield();
-  e = get_nanos();
+  print2(
+    "\n//> Executing %s in %s\n",
+    __FILE__, VERSION);
+  newln1();
+
+  e = nanornd(0xec19);
 
   // select the good ones
   for (int i = 0; i < 256; i++) {
@@ -99,73 +122,67 @@ int main(void) {
     bytes[i] = 0;
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // count the good ones
   *(uint32_t *)nb = 0;
-  for (int i = 0; i < 256; i++) {
+  for (i = 0; i < 256; i++) {
       if(!(c = bytes[i])) continue;
       nb[nbits[c]-3]++;
       n++;
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // check their counting
-  printf("  tot: %3d/124\n", n);
-  printf("   3b: %3d/124\n", nb[0]);
-  printf("   4b: %3d/124\n", nb[1]);
-  printf("   5b: %3d/124\n", nb[2]);
-  putchar('\n');
+  print1("  tot: %3d/124\n", n);
+  print1("   3b: %3d/124\n", nb[0]);
+  print1("   4b: %3d/124\n", nb[1]);
+  print1("   5b: %3d/124\n", nb[2]);
+  newln1();
   if(n != 124) return(1);
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // store the good ones
   n = 1;
   memset(goods, 0, sizeof(goods));
-  for (int i = 0; i < 256; i++) {
+  for (i = 0; i < 256; i++) {
       if(!(c = bytes[i])) continue;
       goods[n++] = c;
       if(n & 0x1F) continue;
       goods[n++] = 0;
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // print the good ones
-  for (int i = 0; i < 4; i++)
-    printf("  idx:  hex, bits%3s| ","");
-  putchar('\n');
-  for (int i = 0; i < 128; i) {
+  for (i = 0; i < 4; i++)
+    print1("  idx:  hex, bits%3s| ","");
+  newln1();
+  for (i = 0; i < 128; i) {
     c = goods[i];
-    printf("  %03d: 0x%02x, %s%2d%-4s| ",
+    print1("  %03d: 0x%02x, %s%2d%-4s| ",
          i, c,
       nbits[c] ? " " : "(",
       nbits[c],
             c  ? " " : ")");
-    if(!(++i & 3)) putchar('\n');
+    if(!(++i & 3)) newln1();
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // create their table
   memset(table, 0, sizeof(table));
   for(int m = 3; m < 6; m++) {
     n = (m-3) << 6;
-    for (int i = 0; i < 128; i++) {
+    for (i = 0; i < 128; i++) {
       if((c = goods[i]) && nbits[c] == m)
         table[n++] = c;
     }
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // spacing their table
   for(i = 8; i < 56; i += 8) {
@@ -174,8 +191,7 @@ int main(void) {
     memset(&table[n-8], 0, 8);
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
+  e = nanornd(e);
 
   // print their table
   for(int m = 3; m < 6; m++) {
@@ -183,34 +199,63 @@ int main(void) {
     n = (m-3) << 6;
     for (int i = 0; i < 64; i++) {
       if(!(c = table[n+i])) continue;
-      if(!(k++ & 3)) putchar('\n');
-      printf("  %03d: b#%08b %d | ",
+      if(!(k++ & 3)) newln1();
+      print1("  %03d: b#%08b %d | ",
         n+i, c, nbits[c]);
     }
-    putchar('\n');
+    newln1();
   }
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
-  
+  e = nanornd(e);
+
   // checking the spacing
   n = 0;
   for(i = 0; i < 64; i++)
     if(!table[64+i] && !table[64+((~i)&63)])
-      printf("%s %03d", n++?",":"  err:", i);
-  if(n) putchar('\n');
+      print1("%s %03d", n++?",":"  err:", i);
+  if(n) newln1();
 
-  sched_yield();
-  e = t ^ (e * (t = get_nanos()));
-  
-  fprintf(stderr,
-    "\n//  time spent: %7lu nS --> %.03lf mS",
-      t, (double)t/1000000);
-  e = (e >> 32) ^ ((e << 32) >> 32);
-  fprintf(stderr,
-    "\n//  entr. pool: 0x%08lx --> b#%032b\n\n",
-      e, e);
-  
-  //putchar('\n');
+  e = nanornd(e);
+
+  for(n = 0; n < (argn?:4); n++) {
+    uint32_t m = 0;
+
+    r = (e >> 32) ^ ((e << 32) >> 32);
+    write4(r);
+    print1(
+      "\n  entr. pool: 0x%08x --> b#%032b\n",
+        r, r);
+    print1("  const. #%d", n);
+
+    i = (r = rotl5(r)) & 31;
+    c = table[1 + i];
+    print1(": %03d", c);
+    m = c << 24;
+
+    i = (r = rotl5(r)) & 63;
+    c = table[64 + i] ?: table[64 + ((~i)&63)];
+    print1(", %03d", c);
+    m |= c << 16;
+
+    i = (r = rotl5(r)) & 31;
+    c = table[129 + i];
+    print1(", %03d", c);
+    m |= c << 8;
+
+    i = (r = rotl5(r)) & 63;
+    c = table[64 + i] ?: table[64 + ((~i)&63)];
+    print1(", %03d", c);
+    m |= c << 0;
+
+    print1(" --> 0x%08x\n", m);
+    e = nanornd((~e) ^ rotl5(r));
+  }
+
+  e = get_nanos();
+  print2(
+    "\n//> Time spent: %7lu nS --> %.03lf mS\n",
+      e, (double)e/1000000);
+
+  newln1();
   return 0;
 }
